78123889ab5ff2a03ecc845d0f60fbcd9f8550ab
enrique esparza & paul morin <student@epicodus.com>
2015-07-22 12:14:44 -0700

    location database and view

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,34 @@
+class LocationsController < ApplicationController
+  def new
+    @location = Location.new
+  end
+
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @locations = Location.find(params[:id])
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      flash[:notice] = "Your location has been added"
+      redirect_to locations_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+    redirect_to locations_path
+  end
+
+  private
+  def question_params
+    params.require(:location).permit(:city)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root :to => 'members#index'
   resources :members
+  resources :locations
 end
